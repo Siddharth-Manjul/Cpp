8a92ccb934b14627ee50fc620d075395676f6f58
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-05-10 23:06:21 +0530

    18th commit

--- binary-decimal.cpp/decimal-to-binary.cpp
+++ binary-decimal.cpp/decimal-to-binary.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main () {
-    int n;
+    long long int n;
     cin >> n;
 
-    int binary[32];
-    int i = 0;
+    // FIRST APPROACH
+    // int binary[32];
+    // int i = 0;
+
+    // while (n!=0) {
+    //     binary[i] = n%2;
+    //     n = n/2;
+    //     i++;
+    // }
+
+    // for (int j = i - 1; j >= 0; j--) {
+    //     cout << binary[j];
+    // }
+
+    // SECOND APPROACH
+
+    unsigned long long int ans = 0;
+    unsigned long long int i = 0;
+
+    if (n < 0) {
+        n = pow(2, 16) + n;
+    }
+    cout << n << endl;
 
     while (n!=0) {
-        binary[i] = n%2;
-        n = n/2;
+        int bit = n&1;
+        ans = (bit * pow(10,i)) + ans;
+        n = n >> 1;
         i++;
     }
+    cout << ans;
+    return 0;
 
-    for (int j = i - 1; j >= 0; j--) {
-        cout << binary[j];
-    }
 
 }
