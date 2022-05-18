34ec676f67ea3e342509cb37416bff424f2cd1cd
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-05-18 23:54:03 +0530

    33rd commit

---
+++ Switch/week.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    
+    int n;
+
+    cout << "Enter the week number to find the corresponding weekday: ";
+    cin >> n;
+
+    switch (n == 1) {
+
+        case 1: cout << "Sunday" << endl; 
+        break;
+
+        case 0: 
+            switch (n == 2) {
+
+                case 1: cout << "Monday" << endl;
+                break;
+
+                case 0:  
+                    switch (n == 3) {
+
+                        case 1: cout << "Tuesday" << endl;
+                        break;
+                        
+                        case 0:
+                            switch (n == 4) {
+
+                                case 1: cout << "Wednesday" << endl;
+                                break;
+
+                                case 0:
+                                    switch (n == 5) {
+
+                                        case 1: cout << "Thursday" << endl;
+                                        break;
+
+                                        case 0: 
+                                            switch (n == 6) {
+
+                                                case 1: cout << "Friday" << endl;
+                                                break;
+
+                                                case 0: cout << "Saturday" << endl;
+                                                break;
+                                            }
+                                            break;
+
+                                    }
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+            }
+            break;
+    }
+}
