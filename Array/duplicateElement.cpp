3635ef2910d99b286dfbb3cd042c8cbc8340709d
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-05-28 22:53:24 +0530

    50th commit

---
+++ Array/duplicateElement.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int duplicateElement(int arr[], int size) {
+
+    int ans = 0;
+
+    for (int i = 0; i < size; i++) {
+        ans = ans^arr[i];
+    }
+
+    for (int i = 0; i < size; i++ ) {
+        ans = ans^i;
+    }
+
+    return ans;
+}
+
+int main () {
+
+    int n;
+
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter the elements of array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << duplicateElement(arr, n) << endl;
+}
